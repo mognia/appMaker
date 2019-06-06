c31676727d1a2c9f66f534deb74d13614a8ae8ff
Mohsen Esmaeili <m-esm@hotmail.com>
2019-06-06 21:12:16 +0430

    README

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var fs = require("fs-extra");
+var mocha = require("gulp-mocha");
+var runSequence = require("run-sequence");
+var child = require("child_process");
+
+var paths = {
+  dist: "dist",
+  tsSources: "src/**/*.ts",
+  tests: "dist/*.test.js"
+};
+
+// compile typescripts
+function build() {
+  if (fs.existsSync(paths.dist)) {
+    fs.emptyDirSync(paths.dist);
+  }
+
+  return gulp
+    .src(paths.tsSources)
+    .pipe(
+      ts({
+        noImplicitAny: false,
+        target: "ES2015",
+        sourceMap: true,
+        module: "CommonJS",
+        baseUrl: ".",
+        paths: {
+          "*": ["node_modules/*", "src/types/*"]
+        }
+      })
+    )
+    .pipe(gulp.dest(paths.dist));
+}
+function handleError(err) {
+  console.log(err.toString());
+  this.emit("end");
+}
+function test() {
+  return gulp.src([paths.tests], { read: false }).pipe(
+    mocha({
+      reporter: "spec",
+      exit: true
+    }).on("error", handleError)
+  );
+}
+
+let server;
+
+function run(done) {
+  if (server) if (server.kill) server.kill();
+
+  let childProcess = child.spawn("node", ["./dist/start.js"], {
+    stdio: "inherit",
+    env: {
+      ...process.env
+    }
+  });
+
+  server = childProcess;
+
+  done();
+}
+
+gulp.watch(paths.tsSources, gulp.series(build, run));
+
+exports.test = gulp.series(build, test, async () => {});
+exports.build = gulp.series(build);
+exports.default = gulp.series(build, run);
